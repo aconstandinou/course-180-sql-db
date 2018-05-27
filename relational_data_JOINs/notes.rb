f852adfc85973b390aa74f77caf79dc825195696
antoniozeus <constandinou.antonio@gmail.com>
2018-05-27 12:17:17 -0500

    sql course notes

--- relational_data_JOINs/notes.rb
+++ relational_data_JOINs/notes.rb
@@ -152,15 +152,12 @@ SELECT customers.id, customers.email, COUNT(DISTINCT tickets.event_id)
   HAVING COUNT(DISTINCT tickets.event_id) = 3;
 
 
+# 7.
 
-
-
-
-
-
-
-
-
-
-
-.
+SELECT events.name AS event, events.starts_at, sections.name AS section, seats.row, seats.number AS seat
+  FROM tickets
+    INNER JOIN events ON tickets.event_id = events.id
+    INNER JOIN customers ON tickets.customer_id = customers.id
+    INNER JOIN seats ON tickets.seat_id = seats.id
+    INNER JOIN sections ON seats.section_id = sections.id
+WHERE customers.email = 'gennaro.rath@mcdermott.co';
